d5127acfbe32e4f95e2123179c67091af8e894ca
Lukman Shaikh <shaikhlukman01@gmail.com>
2019-07-25 03:16:48 +0000

    State my changes
    Signed-off-by: Lukman Shaikh <shaikhlukman01@gmail.com>

--- config.sh
+++ config.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-GITHUB_USER=JarlPenguin
-GITHUB_EMAIL=jarlpenguin@gmail.com
+GITHUB_USER=LukmanShaikh
+GITHUB_EMAIL=shaikhlukman01@gmail.com
 
-KBUILD_BUILD_USER="JarlPenguin"
-KBUILD_BUILD_HOST="builder"
+KBUILD_BUILD_USER="Luqman"
+KBUILD_BUILD_HOST="Unique"
 
-device=montana
+device=land
 
-ROM="CarbonROM"
-manifest_url="https://github.com/CarbonROM/android"
-rom_vendor_name="carbon" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
-branch="cr-7.0"
+ROM="Evolution-X"
+manifest_url="https://github.com/Evolution-X/platform_manifest"
+rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
+branch="pie"
 
-release_repo="JarlPenguin/releases"
+release_repo="LukmanShaikh/releases"
 
 timezone="Seoul"
