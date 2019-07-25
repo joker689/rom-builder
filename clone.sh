274f406517bee03f23fffe87998ff5ba323d9b96
Lukman Shaikh <shaikhlukman01@gmail.com>
2019-07-25 03:05:50 +0000

    add land repos
    Signed-off-by: Lukman Shaikh <shaikhlukman01@gmail.com>

--- clone.sh
+++ clone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # write commands if you want to clone some repos
-mkdir .repo/local_manifests
-curl https://raw.githubusercontent.com/JarlPenguin/manifests/master/Carbon7.xml -o .repo/local_manifests/carbon.xml > /dev/null 2>&1
-repo sync --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all) -q device/motorola/montana vendor/motorola/montana kernel/motorola/msm8937 2>&1 >>logwe 2>&1
+git clone -b lineage-16.0 https://github.com/anshumanxda/android_device_xiaomi_land device/xiaomi/land
+rm -rf vendor/xiaomi && git clone -b lineage-16.0-land https://github.com/RiteshSaxena/proprietary_vendor_xiaomi vendor/xiaomi && cd vendor/xiaomi && git reset --hard HEAD~1 && cd ../..
+git clone -b pie https://github.com/RiteshSaxena/android_kernel_xiaomi_msm8937 kernel/xiaomi/msm8937
