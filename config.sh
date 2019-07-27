Akianonymus/rom_builder

Clone repos from .dependencies file

config.sh
@@ -6,11 +6,12 @@ GITHUB_EMAIL=anonymus.aki@gmail.com
 KBUILD_BUILD_USER="Aki"
 KBUILD_BUILD_HOST="A_DEAD_PLANET"
 
-device=harpia
+export oem=motorola
+export device=harpia
 
 ROM="PixelExperience"
 manifest_url="https://github.com/PixelExperience/manifest"
-rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
+export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
 branch="pie"
 
 release_repo="Akianonymus/harpia_builds"

joker689/rom-builder

config.sh
@@ -6,11 +6,12 @@ GITHUB_EMAIL=shaikhlukman01@gmail.com
 KBUILD_BUILD_USER="Luqman"
 KBUILD_BUILD_HOST="Unique"
 
-device=land
+export oem=xiaomi
+export device=land
 
 ROM="CrDroid"
 manifest_url="https://github.com/crdroidandroid/android"
-rom_vendor_name="lineage" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
+export rom_vendor_name="lineage" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
 branch="9.0"
 
 release_repo="LukmanShaikh/releases"