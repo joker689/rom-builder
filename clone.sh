a232fb4f117bf68ded2527ae617cb1be7127f97a
Lukman Shaikh <shaikhlukman01@gmail.com>
2019-07-29 04:35:22 +0000

    Revert "Change kernel"
    This reverts commit 54ebe61b5f4c9054c534df8f77b633f5c2369507.

--- clone.sh
+++ clone.sh
@@ -4,5 +4,5 @@
 make clean
 git clone -b aosip-p https://github.com/lukmanshaikh/device_xiaomi_land device/xiaomi/land
 rm -rf vendor/xiaomi && git clone -b lineage-16.0-land https://github.com/RiteshSaxena/proprietary_vendor_xiaomi vendor/xiaomi && cd vendor/xiaomi && git reset --hard HEAD~1 && cd ../..
-git clone -b new https://github.com/lukmanshaikh/android_kernel_xiaomi_msm8937-2 kernel/xiaomi/msm8937
+git clone -b pie https://github.com/RiteshSaxena/android_kernel_xiaomi_msm8937 kernel/xiaomi/msm8937
 rm -rf frameworks/base && git clone https://github.com/lukmanshaikh/android_frameworks_base-1 -b 9.0 frameworks/base
